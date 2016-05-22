ac4acd73b2bd6f14777cac88a5b67375d2f2b86a
ArtemkaUser <Artemkauser@Yandex.ru>
2016-05-22 04:51:32 +0300

    Update and rename 123.py to code.py

---
+++ code.py
@@ -0,0 +1,40 @@
+n=int(input())
+p=1
+a=[[0 for s in range(n)] for i in range(n)]
+y=0
+t=n**2
+while p<=t:
+    if p<=t:
+        for i in range(y,y+1):
+           for j in range(y,n-y):
+               a[i][j]=p
+               p+=1
+    else:
+        break
+    if p<=t:
+        for i in range(y+1,n-y-1):
+            for j in range(n-y-1,n-y):
+                a[i][j]=p
+                p+=1
+    else:
+        break
+    if p<=t:
+        for i in range(n-y-1,n-y):
+            for j in range(n-y-1,y,-1):
+                a[i][j]=p
+                p+=1
+    else:
+        break
+    if p<=t:
+        for i in range(n-y-1,y,-1):
+            for j in range(y,y+1):
+                a[i][j]=p
+                p+=1
+    else:
+        break
+    y+=1
+print('')
+for row in a:
+    for elem in row:
+        print(elem, end='\t')
+    print()
